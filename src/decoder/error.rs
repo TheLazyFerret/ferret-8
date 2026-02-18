0fa2d16d09304fcb09bcb662ce5491e2ae4794b7
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-02-18 16:43:40 +0000

    remove unused NotImplemented from decoder error

--- src/decoder/error.rs
+++ src/decoder/error.rs
@@ -6,14 +6,12 @@ use std::fmt;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum DecodeError {
-  NotImplemented(u16),
   Unknown(u16),
 }
 
 impl fmt::Display for DecodeError {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     match self {
-      | Self::NotImplemented(n) => write!(f, "This instruction has not been implemented 0x{:X}", n),
       | Self::Unknown(n) => write!(f, "Unkown instruction 0x{:X}", n),
     }
   }
