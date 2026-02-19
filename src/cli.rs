53bc8de56ba539e35279d1eccd49596cfabdffa0
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-02-19 09:01:21 +0000

    add clap default values for cycles and upscale factor

--- src/cli.rs
+++ src/cli.rs
@@ -6,28 +6,24 @@ use std::sync::RwLock;
 use clap::Parser;
 
 pub static PROGRAM_NAME: RwLock<String> = RwLock::new(String::new());
-pub static CYCLES: RwLock<usize> = RwLock::new(700);
-pub static UPSCALE_FACTOR: RwLock<usize> = RwLock::new(20);
+pub static CYCLES: RwLock<usize> = RwLock::new(0);
+pub static UPSCALE_FACTOR: RwLock<usize> = RwLock::new(0);
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
   #[arg(short, long)]
   program: String,
-  #[arg(short, long)]
-  cycles: Option<usize>,
-  #[arg(short, long)]
-  upscale_factor: Option<usize>,
+  #[arg(short, long, default_value_t = 700)]
+  cycles: usize,
+  #[arg(short, long, default_value_t = 20)]
+  upscale_factor: usize,
 }
 
 /// Parse the command arguments of the program.
 pub fn parse_arguments() {
   let args = Args::parse();
   *PROGRAM_NAME.try_write().unwrap() = args.program;
-  if args.cycles.is_some() {
-    *CYCLES.try_write().unwrap() = args.cycles.unwrap();
-  }
-  if args.upscale_factor.is_some() {
-    *CYCLES.try_write().unwrap() = args.upscale_factor.unwrap();
-  }
+  *CYCLES.try_write().unwrap() = args.cycles;
+  *UPSCALE_FACTOR.try_write().unwrap() = args.upscale_factor;
 }
